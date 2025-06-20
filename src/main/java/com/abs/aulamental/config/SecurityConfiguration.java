ca5f8b7b4739af18265fdd4dbd8d14b541897d8a
sebastiangulod <sebastian3000ad@gmail.com>
2025-06-20 12:53:04 -0500

    a

--- src/main/java/com/abs/aulamental/config/SecurityConfiguration.java
+++ src/main/java/com/abs/aulamental/config/SecurityConfiguration.java
@@ -24,6 +24,7 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfiguration {
 
 
+
     private  final SecurityFilter securityFilter;
 
     @Autowired
